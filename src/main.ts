bed9eec670417de76ac093c89ad9b177e4db7dbe
hyoseok <janghs0604@gmail.com>
2022-06-28 07:20:57 +0900

    Fix: Transform exception for double nested object

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,21 @@ async function bootstrap() {
       transform: true,
       transformOptions: { enableImplicitConversion: true },
       exceptionFactory: (errors) => {
+        // TODO: Fix for 3, 4... nested object, only working object and double nested object
         const errorObject = errors.reduce((errorObj, error) => {
           if (errorObj[error.property] === undefined)
             errorObj[error.property] = [];
-          errorObj[error.property].push(...Object.values(error.constraints));
+
+          if (error.children) {
+            errorObj[error.property].push(
+              ...error.children.reduce((acc, value) => {
+                acc.push(...Object.values(value.constraints));
+                return acc;
+              }, []),
+            );
+          } else {
+            errorObj[error.property].push(...Object.values(error.constraints));
+          }
           return errorObj;
         }, {});
         return new BadRequestException(errorObject);
