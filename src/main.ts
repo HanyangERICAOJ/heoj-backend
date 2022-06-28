d79a66bcfc9d025bfe14ad263e151d54f5e7b3eb
hyoseok <janghs0604@gmail.com>
2022-06-28 14:31:09 +0900

    Fix: Fix not working

--- src/main.ts
+++ src/main.ts
@@ -9,22 +9,26 @@ async function bootstrap() {
     new ValidationPipe({
       transform: true,
       transformOptions: { enableImplicitConversion: true },
+      stopAtFirstError: true,
       exceptionFactory: (errors) => {
         // TODO: Fix for 3, 4... nested object, only working object and double nested object
         const errorObject = errors.reduce((errorObj, error) => {
           if (errorObj[error.property] === undefined)
             errorObj[error.property] = [];
 
-          if (error.children) {
+          if (error.children.length > 0) {
             errorObj[error.property].push(
               ...error.children.reduce((acc, value) => {
                 acc.push(...Object.values(value.constraints));
                 return acc;
               }, []),
             );
-          } else {
+          }
+
+          if (error.constraints) {
             errorObj[error.property].push(...Object.values(error.constraints));
           }
+
           return errorObj;
         }, {});
         return new BadRequestException(errorObject);
