42fb04adb4b66909a445eb485b2ea6cd99dac76d
hyoseok <janghs0604@gmail.com>
2022-07-04 16:20:23 +0900

    Fix: Remove unused DTO field

--- src/problems/dtos/update-problem-examples.dto.ts
+++ src/problems/dtos/update-problem-examples.dto.ts
@@ -1,13 +1,5 @@
-import { Transform, Type } from 'class-transformer';
-import { IsNotEmpty, Validate } from 'class-validator';
-import { NumberExistConstraint } from '../constraints/number-exist.constraint';
 import { Example } from '../interfaces/example.interface';
 
 export class UpdateProblemExampleDTO {
-  @IsNotEmpty()
-  @Type(() => Number)
-  @Validate(NumberExistConstraint)
-  number: number;
-
   examples: Example[];
 }
