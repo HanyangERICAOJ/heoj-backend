acb12ae90d88213fc9ef5122e71b4e5551136fab
hyoseok <janghs0604@gmail.com>
2022-07-04 16:56:37 +0900

    Feat: Create submissions module

--- src/submissions/submissions.module.ts
+++ src/submissions/submissions.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Submission } from './entities/submission.entity';
+import { SubmissionsService } from './submissions.service';
+import { SubmissionsController } from './submissions.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Submission])],
+  providers: [SubmissionsService],
+  controllers: [SubmissionsController],
 })
 export class SubmissionsModule {}
